00d6f09278483c4693fab09415454339f9f3d8d0
johnkuefler <john@dev-squared.com>
2023-04-11 00:37:38 +0000

    Added the rest of the auth sample code

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const personController = require('../controllers/personController.js')
+const authMiddleware = require('../middleware/auth');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', authMiddleware.ensureAuthenticated, function(req, res, next) {
   personController.getall(req, res);
 });
 
-router.get('/create', function(req, res, next) {
+router.get('/create', authMiddleware.ensureAuthenticated, function(req, res, next) {
   res.render('create');
 });
 
-router.get('/update', function(req, res, next) {
-  personController.update_get(req,res);
+router.get('/update', authMiddleware.ensureAuthenticated, function(req, res, next) {
+  personController.update_get(req, res);
 });
 
-router.post('/update', function(req, res, next) {
-  personController.update(req,res);
+router.post('/update', authMiddleware.ensureAuthenticated, function(req, res, next) {
+  personController.update(req, res);
 });
 
-router.get('/delete', function(req, res, next) {
+router.get('/delete', authMiddleware.ensureAuthenticated, function(req, res, next) {
   personController.delete(req, res);
 });
 
